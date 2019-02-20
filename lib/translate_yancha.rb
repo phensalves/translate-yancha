296a0a6fdb437c6288af9dfaefb4a35100f458d0
Pedro Alves <pedro_alves@me.com>
2019-02-20 13:12:21 -0300

    Initial commit

---
+++ lib/translate_yancha.rb
@@ -0,0 +1,6 @@
+require "translate_yancha/version"
+
+module TranslateYancha
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/translate_yancha/version.rb
@@ -0,0 +1,3 @@
+module TranslateYancha
+  VERSION = "0.1.0"
+end
