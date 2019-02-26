0cd435e82ff1420e19bf416acfe553816d931af8
Pedro Alves <pedro_alves@me.com>
2019-02-26 12:30:29 -0300

    adding tests (with TDD)

--- spec/translate_yancha_spec.rb
+++ spec/translate_yancha_spec.rb
@@ -1,9 +1,24 @@
-RSpec.describe TranslateYancha do
+require 'spec_helper'
+describe "GET api/v1.5/tr.json/translate (Translate)" do
   it "has a version number" do
     expect(TranslateYancha::VERSION).not_to be nil
   end
 
   it "does something useful" do
     expect(false).to eq(true)
+  end
+
+  it "Api response == 200" do
+    textPt = "Nós estamos testando o tradutor"
+    yandexApi = TranslateYancha::Yancha.new YANCHA_TEST_API_KEY
+    response = yandexApi.translate textPt, "en"
+    expect(response["code"]).to eq(200)
+  end
+
+  it "Api response == 200" do
+    textPt = "Nós estamos testando o tradutor"
+    yandexApi = TranslateYancha::Yancha.new YANCHA_TEST_API_KEY
+    response = yandexApi.translate textPt, "en"
+    expect(response["code"]).to eq(200)
   end
 end
