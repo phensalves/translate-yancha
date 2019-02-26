c62e3104eb2d37685c56d4e4e30e7ae7fa349d5f
Pedro Alves <pedro_alves@me.com>
2019-02-26 15:30:48 -0300

    fix call and finish implementation

--- lib/translate_yancha.rb
+++ lib/translate_yancha.rb
@@ -1,6 +1,35 @@
-require "translate_yancha/version"
+require 'translate_yancha/version'
+require 'rest-client'
+require 'json'
 
 module TranslateYancha
-  class Error < StandardError; end
-  # Your code goes here...
+  class Yancha
+
+    def initialize(api_key)
+      @api_key = api_key
+    end
+
+    def translate text, language
+      result =  RestClient.get ENV['YANDEX_TRANSLATE_URL'], {params: {key: @api_key, 
+                                                                      text: text, 
+                                                                      format: 'plain', 
+                                                                      lang: language
+                                                                    }
+                                                            }
+      JSON.parse(result.body)
+    end
+
+    def list
+      result =  RestClient.get ENV['YANDEX_GET_LANGS_URL'], {params: {key: @api_key}}
+      JSON.parse(result.body)
+    end
+
+    def detect text
+      result =  RestClient.get ENV['YANDEX_DETECT_URL'], {params: { key: @api_key, 
+                                                                    text: text
+                                                                  }
+                                                          }
+      JSON.parse(result.body)
+    end
+  end
 end
--- spec/translate_yancha_spec.rb
+++ spec/translate_yancha_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe TranslateYancha do
   describe 'GET api/v1.5/tr.json/translate (Translate)' do
     it 'Api response == 200' do
       textPt = 'Nós estamos testando o tradutor'
-      yandexApi = TranslateYancha::Yandex.new YANCHA_TEST_API_KEY
-      response = yandexApi.translate textPt, 'en'
+      yanchaApi = TranslateYancha::Yancha.new ENV['YANCHA_TEST_API_KEY']
+      response = yanchaApi.translate textPt, 'en'
       expect(response['code']).to eq(200)
     end
 
     it 'translated text match' do
       textPt = 'Nós estamos testando o tradutor'
       textEn = 'We are testing the translator'
-      yandexApi = TranslateYancha::Yandex.new YANCHA_TEST_API_KEY
-      response = yandexApi.translate textPt, 'en'
+      yanchaApi = TranslateYancha::Yancha.new ENV['YANCHA_TEST_API_KEY']
+      response = yanchaApi.translate textPt, 'en'
 
       expect(response['text'].first).to eq(textEn)
     end
@@ -22,10 +22,27 @@ RSpec.describe TranslateYancha do
   # Teste 2 (supported list of languages)
   describe "GET api/v1.5/tr.json/getLangs (Supported List)" do
     it "Api response not equal nil or empty '' " do
-      yandexApi = TranslateYandex::Yandex.new YANCHA_TEST_API_KEY
-      response = yandexApi.list
+      yanchaApi = TranslateYancha::Yancha.new ENV['YANCHA_TEST_API_KEY']
+      response = yanchaApi.list
       expect(response).not_to eq(nil)
       expect(response).not_to eq("")
+    end
+  end
+
+  # Teste 3 (Language Detect)
+  describe "GET api/v1.5/tr.json/detect (Detect)" do
+    it "Api response == 200" do
+      textPt = "Nós estamos testando o tradutor"
+      yanchaApi = TranslateYancha::Yancha.new ENV['YANCHA_TEST_API_KEY']
+      response = yanchaApi.detect textPt
+      expect(response["code"]).to eq(200)
+    end
+
+    it "detect language match with right language" do
+      textPt = "Nós estamos testando o tradutor"
+      yanchaApi = TranslateYancha::Yancha.new ENV['YANCHA_TEST_API_KEY']
+      response = yanchaApi.detect textPt
+      expect(response["lang"]).to eq("pt")
     end
   end
 end
