d42af25d4afb2b34c02d2326d8a1527eb775e977
Pedro Alves <pedro_alves@me.com>
2019-02-26 14:55:31 -0300

    applying some new tests

--- spec/translate_yancha_spec.rb
+++ spec/translate_yancha_spec.rb
@@ -1,19 +1,31 @@
 require 'spec_helper'
-RSpec.describe 'GET api/v1.5/tr.json/translate (Translate)' do
+RSpec.describe TranslateYancha do
   # Teste 1 (Translate)
-  it 'Api response == 200' do
-    textPt = 'Nós estamos testando o tradutor'
-    yandexApi = TranslateYandex::Yandex.new TEST_API_KEY
-    response = yandexApi.translate textPt, 'en'
-    expect(response['code']).to eq(200)
+  describe 'GET api/v1.5/tr.json/translate (Translate)' do
+    it 'Api response == 200' do
+      textPt = 'Nós estamos testando o tradutor'
+      yandexApi = TranslateYancha::Yandex.new YANCHA_TEST_API_KEY
+      response = yandexApi.translate textPt, 'en'
+      expect(response['code']).to eq(200)
+    end
+
+    it 'translated text match' do
+      textPt = 'Nós estamos testando o tradutor'
+      textEn = 'We are testing the translator'
+      yandexApi = TranslateYancha::Yandex.new YANCHA_TEST_API_KEY
+      response = yandexApi.translate textPt, 'en'
+
+      expect(response['text'].first).to eq(textEn)
+    end
   end
 
-  it 'translated text match' do
-    textPt = 'Nós estamos testando o tradutor'
-    textEn = 'We are testing the translator'
-    yandexApi = TranslateYandex::Yandex.new TEST_API_KEY
-    response = yandexApi.translate textPt, 'en'
-
-    expect(response['text'].first).to eq(textEn)
+  # Teste 2 (supported list of languages)
+  describe "GET api/v1.5/tr.json/getLangs (Supported List)" do
+    it "Api response not equal nil or empty '' " do
+      yandexApi = TranslateYandex::Yandex.new YANCHA_TEST_API_KEY
+      response = yandexApi.list
+      expect(response).not_to eq(nil)
+      expect(response).not_to eq("")
+    end
   end
 end
